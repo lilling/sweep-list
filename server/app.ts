fb79f478e4f7a7e973bba05571579f6369aba026
yonatanli <yonatanl@razorgrip.com>
2018-02-12 00:53:30 +0200

    add dbInit class

--- server/app.ts
+++ server/app.ts
@@ -2,12 +2,12 @@ import 'reflect-metadata';
 import * as bodyParser from 'body-parser';
 import * as express from 'express';
 import * as https from 'https';
-import {createConnection} from "typeorm";
+
 import * as http from 'http';
 import * as fs from 'fs';
 import * as compression from 'compression';
 import * as forceSsl from 'express-force-ssl';
-import { User } from './db/entity/User';
+import { DbInitilizer } from './db/dbInit';
 //
 
 export class App {
@@ -17,7 +17,7 @@ export class App {
 
     public static bootstrap(): App {
         const app = new App();
-        this.initDB();
+        DbInitilizer.initDB();
         return app;
     }
     constructor() {
@@ -32,25 +32,7 @@ export class App {
         this.express = express();
     }
 
-    private static initDB() {
-        createConnection().then(async connection => {
 
-            console.log("Inserting a new user into the database...");
-            const user = new User();
-            user.firstName = "Timber";
-            user.lastName = "Saw";
-            user.age = 25;
-            await connection.manager.save(user);
-            console.log("Saved a new user with id: " + user.id);
-
-            console.log("Loading users from the database...");
-            const users = await connection.manager.find(User);
-            console.log("Loaded users: ", users);
-
-            console.log("Here you can setup and run express/koa/any other framework.");
-
-        }).catch(error => console.log(error));
-    }
 
     private middleware(): void {
         this.express.use(forceSsl);
