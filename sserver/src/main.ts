a6dd269e853ca756786218289aeebd6453724897
yonatanli <yonatanl@razorgrip.com>
2018-02-15 00:22:17 +0200

    ad ssl

--- sserver/src/main.ts
+++ sserver/src/main.ts
@@ -1,11 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { ApplicationModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {ApplicationModule} from './app.module';
 import * as path from 'path';
 import * as express from 'express';
+import * as fs from 'fs';
+import * as https from 'https';
+import * as http from 'http';
 
 async function bootstrap() {
-    const app = await NestFactory.create(ApplicationModule);
+    const httpsOptions = {
+        key: fs.readFileSync('./ssl/localhost.key'),
+        cert: fs.readFileSync('./ssl/localhost.crt'),
+    };
+    const server = express();
+    const app = await NestFactory.create(ApplicationModule, server);
     app.use(express.static(path.join(__dirname, '../../dist')));
-    await app.listen(3000);
+    await app.init();
+
+    http.createServer(server).listen(3000);
+    https.createServer(httpsOptions, server).listen(443);
 }
+
 bootstrap();
