32cd31f81d2f782b8f7643d2517f49a16c8e8175
yonatanli <yonatanl@razorgrip.com>
2018-02-15 00:06:24 +0200

    serve angular

--- sserver/src/main.ts
+++ sserver/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
+import * as path from 'path';
+import * as express from 'express';
 
 async function bootstrap() {
-	const app = await NestFactory.create(ApplicationModule);
-	await app.listen(3000);
+    const app = await NestFactory.create(ApplicationModule);
+    app.use(express.static(path.join(__dirname, '../../dist')));
+    await app.listen(3000);
 }
 bootstrap();
