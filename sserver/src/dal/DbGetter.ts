40c2a25fab9a984f9257934d51640f7c8592f70f
Dovi Lilling <dovi.lilling+SweepImp@gmail.com>
2018-03-04 15:11:31 +0200

    Change PostgreSQL to Heroku

--- sserver/src/dal/DbGetter.ts
+++ sserver/src/dal/DbGetter.ts
@@ -4,11 +4,12 @@ import { IDatabase } from 'pg-promise';
 import * as pgMonitor from 'pg-monitor';
 const pgOptions = {};
 const pgConnect = {
-    host: 'hard-plum.db.elephantsql.com',
+    host: 'ec2-79-125-110-209.eu-west-1.compute.amazonaws.com',
     port: 5432,
-    database: 'hsrbfqoh',
-    user: 'hsrbfqoh',
-    password: 'OASQR0O7qDLnHlIBgWcHiD_dKwz4dBoM',};
+    database: 'd3aah4o2s9brq6',
+    user: 'uatfbjfsevdoty',
+    password: 'c0b2db4f5ee255f2510523d840db63919ee59b6f58a24f3f0f7b7b09e917947a',
+    ssl: true,};
 
 export class DbGetter {
     private static db: IDatabase<any>;
