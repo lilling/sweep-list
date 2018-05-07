dd8d28b6f4ffd784020ba0c6d9194a817b03ea9b
yonatanli <yonatan.lilling@cyberbit.com>
2018-05-08 00:15:58 +0300

    open specific sweep

--- src/app/sweep-list/sweep-list.component.ts
+++ src/app/sweep-list/sweep-list.component.ts
@@ -37,4 +37,14 @@ export class SweepListComponent implements OnInit {
     addSweep() {
         this.dialog.open(AddSweepComponent);
     }
+
+    openUrl(urlToOpen: string) {
+        let url: string = '';
+        if (!/^http[s]?:\/\//.test(urlToOpen)) {
+            url += 'http://';
+        }
+
+        url += urlToOpen;
+        window.open(url, '_blank');
+    }
 }
