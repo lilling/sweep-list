398c82ebb2ec827f24d6991c3a6af312d0605378
lilling <yonatanliling@gmail.com>
2018-06-29 00:00:38 +0300

    progress

--- src/app/state/sweeps/sweeps.state.ts
+++ src/app/state/sweeps/sweeps.state.ts
@@ -5,10 +5,16 @@ export interface SweepsState {
     sweeps: HashedArray<user_sweep>;
     isSweepsLoading: boolean;
     isAllSweepsLoaded: boolean;
+    mode: SweepsMode;
 }
 
 export const INITIAL_SWEEPS_STATE: SweepsState = {
     sweeps: new HashedArray<user_sweep>([], 'user_sweep_id'),
     isSweepsLoading: true,
     isAllSweepsLoaded: false,
+    mode: undefined
 };
+
+export enum SweepsMode {
+    active, today, tomorrow, later
+}
