0cbf1280de459e48e8df40f371c38d66000e7121
lilling <yonatanliling@gmail.com>
2018-08-09 01:00:19 +0300

    revert

--- sserver/src/main.ts
+++ sserver/src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
     await app.init();
 
     http.createServer(server).listen(3000);
-    https.createServer(httpsOptions, server).listen(8080);
+    https.createServer(httpsOptions, server).listen(443);
 }
 
 bootstrap();
