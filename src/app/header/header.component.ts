d87dd358c9cc938332ae9cbff9b50f797f33fe78
Dovi Lilling <dovi.lilling+SweepImp@gmail.com>
2018-09-20 19:50:10 +0300

    Wins page 1.1 #73

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -23,7 +23,7 @@ export class HeaderComponent {
         iconRegistry.addSvgIcon('feedback', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/feedback.svg'));
         iconRegistry.addSvgIcon('power', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/power.svg'));
         iconRegistry.addSvgIcon('star', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/star.svg'));
-        iconRegistry.addSvgIcon('win', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/win.svg'));
+        iconRegistry.addSvgIcon('star-list', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/star-list.svg'));
     }
 
     toggleSideNav() {
--- src/app/wins/wins.component.ts
+++ src/app/wins/wins.component.ts
@@ -7,7 +7,7 @@ import { NgRedux, select } from '@angular-redux/store';
 import { combineLatest } from 'rxjs/observable/combineLatest';
 //
 import { AddSweepComponent } from '../add-sweep/add-sweep.component';
-import { user_sweep, user_account } from '../../../shared/classes';
+import { user_sweep, user_account, Win } from '../../../shared/classes';
 import { AppState } from '../state/store';
 import { SweepsActions } from '../state/sweeps/sweeps.actions';
 import { SweepsMode } from '../state/sweeps/sweeps.state';
@@ -23,107 +23,26 @@ import { Subscriber } from '../classes/subscriber';
 export class WinsComponent extends Subscriber implements OnInit {
     @select((state: AppState) => state.sweepsState.isSweepsLoading)
     isSweepsLoading$: Observable<boolean>;
-    user: user_account;
-    sweeps: {
-        data: user_sweep,
-        text: string
-    }[];
-    Date = Date;
     userAccountId: AAGUID;
+    userWins: Win[];
 
-    constructor(public dialog: MatDialog,
-                public router: Router,
-                public sweepsService: SweepsService,
-                public sweepsActions: SweepsActions,
-                private ngRedux: NgRedux<AppState>) {
+    constructor(public sweepsService: SweepsService) {
         super();
     }
 
     ngOnInit() {
         this.userAccountId = localStorage.getItem(LocalStorageKeys.loggedUser);
-        this.sweepsActions.goToSweeps(SweepsMode.active);
-
-        this.subscriptions = {
-            sweepsMode: combineLatest(this.ngRedux.select(state => state.sweepsState.mode),
-                this.ngRedux.select(state => state.loginState.user)).subscribe(([mode, user]) => {
-                if (mode === undefined || !user) {
-                    return;
-                }
-                this.user = user;
-                if (mode === SweepsMode.active && !this.ngRedux.getState().sweepsState.sweeps.length()) {
-                    this.sweepsActions.getSweeps({
-                        user_account_id: this.userAccountId,
-                        enabled_social_media_bitmap: user.enabled_social_media_bitmap
-                    }, mode);
-                }
-            }),
-            sweeps: this.ngRedux.select(state => state.sweepsState.sweeps).subscribe(sweeps => {
-                this.sweeps = sweeps.array.reduce((result, current) => {
-                    const element = {
-                        data: current,
-                        text: `${this.getTimeToEnd(new Date(current.end_date).getTime())}`
-                    };
-                    result.push(element);
-                    return result;
-                }, []);
-            })
-        };
-        this.addScrollEventHandler();
+        this.sweepsService.getUserWins(this.userAccountId).subscribe(wins => this.userWins = wins);
     }
 
-    addSweep() {
-        this.dialog.open(AddSweepComponent);
-    }
-
-    nextVisit(sweep: user_sweep) {
-        const nextVisit = sweep.frequency_days * 864e5 - (Date.now() - sweep.last_entry_date.getTime());
-
-        if (nextVisit < 0) {
-            return 'You should enter right now';
+    getWinValueText(wins: number):string{
+        if (wins > 0) {
+            return `$${wins} total`;
         }
-
-        let returnValue = '';
-
-        const days = nextVisit / 864e5;
-
-        if (days > 1) {
-            returnValue = `${days.toFixed(0)} days`;
-        } else {
-            const hours = nextVisit / 36e5;
-            if (hours > 1) {
-                returnValue = `${hours.toFixed(0)} hours`;
-            } else {
-                const minutes = nextVisit / 6e4;
-
-                returnValue = `${minutes.toFixed(0)} minutes`;
-            }
-        }
-
-        return `${returnValue} left to next visit.`;
+        return `No prize values`;
     }
 
-    private getTimeToEnd(endDate: number): string {
-        const diff = endDate - Date.now();
-        let returnValue = '';
-        const days = diff / 864e5;
-
-        if (days > 1) {
-            returnValue = `${days.toFixed(0)} days`;
-        } else {
-            const hours = diff / 36e5;
-            if (hours > 1) {
-                returnValue = `${hours.toFixed(0)} hours`;
-            } else {
-                const minutes = diff / 6e4;
-
-                returnValue = `${minutes.toFixed(0)} minutes`;
-            }
-        }
-
-        return `${returnValue} left`;
-    }
-
-    private addScrollEventHandler(): void {
+    /*private addScrollEventHandler(): void {
         const tableBody = document.getElementsByClassName('body');
         const el = <HTMLDivElement>tableBody[0];
         // some times we get negative scroll position, so we don't want to trigger load more data when scrolling back up
@@ -147,5 +66,5 @@ export class WinsComponent extends Subscriber implements OnInit {
                 lastScroll = currentScroll;
             };
         }
-    }
+    }*/
 }
