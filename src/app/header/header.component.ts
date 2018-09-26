620689c1ea3e9df883a0e895a5f4b2e389d2f87a
lilling <yonatanliling@gmail.com>
2018-09-26 23:22:45 +0300

    header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
-import { MatIconRegistry } from '@angular/material';
 //
 import { CommonActions } from '../state/common/common.actions';
 
@@ -13,17 +11,7 @@ export class HeaderComponent {
     @Input()
     title: string;
 
-    constructor(iconRegistry: MatIconRegistry,
-                sanitizer: DomSanitizer,
-                private commonActions: CommonActions) {
-        iconRegistry.addSvgIcon('calendar', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/calendar.svg'));
-        iconRegistry.addSvgIcon('gift', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/gift.svg'));
-        iconRegistry.addSvgIcon('win', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/win.svg'));
-        iconRegistry.addSvgIcon('settings', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/settings.svg'));
-        iconRegistry.addSvgIcon('feedback', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/feedback.svg'));
-        iconRegistry.addSvgIcon('power', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/power.svg'));
-        iconRegistry.addSvgIcon('star', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/star.svg'));
-        iconRegistry.addSvgIcon('star-list', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/star-list.svg'));
+    constructor(private commonActions: CommonActions) {
     }
 
     toggleSideNav() {
